9ed5a2df2501729fb3eaa1dd8829122698f28aaa
rchilak1 <127245405+rchilak1@users.noreply.github.com>
2024-05-04 11:19:12 -0500

    Update display.py

--- display.py
+++ display.py
@@ -28,7 +28,7 @@ def load_eeg_data(base_dir):
     return np.array(X), np.array(y)
 
 # Load the EEG data
-base_dir = '/Users/rahulc/Downloads/EEG_Epilepsy_Datasets'
+base_dir = 'EEG_Epilepsy_Datasets'
 X, y = load_eeg_data(base_dir)
 
 # Load y_test
